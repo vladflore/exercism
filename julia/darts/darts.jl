bbcb51b546f70327ea715b2056aea3fc69dc904a
Vlad Flore <flore.vlad@gmail.com>
2020-09-30 19:12:00 +0200

    finish ex darts

--- julia/darts/darts.jl
+++ julia/darts/darts.jl
@@ -1,3 +1,7 @@
 function score(x, y)
-
+    distance_to_center = √(x^2 + y^2)
+    if 0 <= distance_to_center <= 1 return 10 end
+    if 1 < distance_to_center <= 5 return 5 end
+    if 5 < distance_to_center <= 10 return 1 end
+    return 0 
 end
