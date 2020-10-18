78438ca1d2c7b4f2dc6715a56287ddb8c20a0013
Vlad Flore <flore.vlad@gmail.com>
2020-10-18 21:54:28 +0200

    add solution for java diff of squares

---
+++ java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
@@ -0,0 +1,15 @@
+class DifferenceOfSquaresCalculator {
+
+    int computeSquareOfSumTo(int n) {
+        return (int) Math.pow(n * (n + 1) / 2.0, 2);
+    }
+
+    int computeSumOfSquaresTo(int n) {
+        return n * (n + 1) * (2 * n + 1) / 6;
+    }
+
+    int computeDifferenceOfSquares(int input) {
+        return computeSquareOfSumTo(input) - computeSumOfSquaresTo(input);
+    }
+
+}
