e1c5d0096126225e6d22e9d36077f3e53ea57245
Vlad Flore <flore.vlad@gmail.com>
2020-10-31 11:11:41 +0100

    Return copy of data in row

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -4,7 +4,7 @@ class Matrix:
                        for line in matrix_string.splitlines()]
 
     def row(self, index):
-        return self.matrix[index-1]
+        return self.matrix[index-1].copy()
 
     def column(self, index):
         return [row[index-1] for row in self.matrix]
