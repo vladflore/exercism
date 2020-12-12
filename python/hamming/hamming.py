8050e36c6485581109c0d57c4707f330b3c1e056
Vlad Flore <flore.vlad@gmail.com>
2020-12-12 16:17:42 +0100

    Just playing around with commit cmd

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -2,3 +2,5 @@ def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError("strands cannot have different lengths")
     return sum([1 if nucleotide != strand_b[idx] else 0 for idx, nucleotide in enumerate(strand_a)])
+
+
