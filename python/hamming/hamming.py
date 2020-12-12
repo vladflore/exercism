677adb72d0bde8f1fe227008eac1d6b55acb3ab7
Vlad Flore <flore.vlad@gmail.com>
2020-12-12 19:35:19 +0100

    Remove extra blank lines

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -2,5 +2,3 @@ def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError("strands cannot have different lengths")
     return sum([1 if nucleotide != strand_b[idx] else 0 for idx, nucleotide in enumerate(strand_a)])
-
-
