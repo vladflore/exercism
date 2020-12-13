386a38031ee1e74b63f8a7bbbb5543f2b91841f4
Vlad Flore <flore.vlad@gmail.com>
2020-12-13 10:07:17 +0100

    Improve first sol after review for hamming in py

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,4 +1,4 @@
 def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError("strands cannot have different lengths")
-    return sum([1 if nucleotide != strand_b[idx] else 0 for idx, nucleotide in enumerate(strand_a)])
+    return sum([n_a != n_b for n_a, n_b in zip(strand_a, strand_b)])
