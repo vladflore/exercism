e9895bdfdd3944767872778f78501015e5a9b0b8
Vlad Flore <flore.vlad@gmail.com>
2020-12-16 23:49:18 +0100

    Refactor solution after review and add new sol

--- julia/secret-handshake/secret-handshake.jl
+++ julia/secret-handshake/secret-handshake.jl
@@ -1,7 +1,7 @@
 function secret_handshake(code)
     mask = 1
-    ans = []
-    for (index, value) in enumerate(["wink","double blink","close your eyes","jump"])
+    ans = String[]
+    for (index, value) in enumerate(("wink", "double blink", "close your eyes", "jump"))
         if (code & mask << (index - 1)) != 0 
             push!(ans, value)
         end
@@ -10,4 +10,14 @@ function secret_handshake(code)
         reverse!(ans)
     end
     ans
+end
+
+function secret_handshake3(code::Integer)
+    handshake = String[]
+    (code >> 0) & 1 == 1 && push!(handshake, "wink")
+    (code >> 1) & 1 == 1 && push!(handshake, "double blink")
+    (code >> 2) & 1 == 1 && push!(handshake, "close your eyes")
+    (code >> 3) & 1 == 1 && push!(handshake, "jump")
+    (code >> 4) & 1 == 1 && reverse!(handshake)
+    return handshake
 end
