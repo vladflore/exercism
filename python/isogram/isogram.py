f4f020e92a046032ed8e70b4ebcfa2d693e85a36
Vlad Flore <flore.vlad@gmail.com>
2020-12-20 23:09:06 +0100

    Refactor solution 4 isogram in py after review

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,8 +1,18 @@
-def is_isogram(string):
+def is_isogram_initial(string):
     letters = set()
     included = 0
     for letter in string:
-        if letter != '-' and letter != ' ':
+        if letter.isalpha():
             letters.add(letter.lower())
             included += 1
     return included == len(letters)
+
+
+def is_isogram(string):
+    visited = []
+    for letter in string.lower():
+        if letter in visited:
+            return False
+        if letter.isalpha():
+            visited.append(letter)
+    return True
