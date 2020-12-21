3d607f02c979689ae111a7811d393ba21595bd95
Vlad Flore <flore.vlad@gmail.com>
2020-12-21 17:27:52 +0100

    Refactor solution for 12d in py

--- python/twelve-days/twelve_days.py
+++ python/twelve-days/twelve_days.py
@@ -1,36 +1,23 @@
 def recite(start_verse, end_verse):
-    days = {
-        0: "first",
-        1: "second",
-        2: "third",
-        3: "fourth",
-        4: "fifth",
-        5: "sixth",
-        6: "seventh",
-        7: "eighth",
-        8: "ninth",
-        9: "tenth",
-        10: "eleventh",
-        11: "twelfth",
+    gift_for_day = {
+        1: ("first", "a Partridge in a Pear Tree"),
+        2: ("second", "two Turtle Doves"),
+        3: ("third", "three French Hens"),
+        4: ("fourth", "four Calling Birds"),
+        5: ("fifth", "five Gold Rings"),
+        6: ("sixth", "six Geese-a-Laying"),
+        7: ("seventh", "seven Swans-a-Swimming"),
+        8: ("eighth", "eight Maids-a-Milking"),
+        9: ("ninth", "nine Ladies Dancing"),
+        10: ("tenth", "ten Lords-a-Leaping"),
+        11: ("eleventh", "eleven Pipers Piping"),
+        12: ("twelfth", "twelve Drummers Drumming")
     }
-    all_presents = [
-        "a Partridge in a Pear Tree",
-        "two Turtle Doves",
-        "three French Hens",
-        "four Calling Birds",
-        "five Gold Rings",
-        "six Geese-a-Laying",
-        "seven Swans-a-Swimming",
-        "eight Maids-a-Milking",
-        "nine Ladies Dancing",
-        "ten Lords-a-Leaping",
-        "eleven Pipers Piping",
-        "twelve Drummers Drumming",
-    ]
     ans = []
-    for d in range(start_verse-1, end_verse):
-        day_presents = ', '.join(
-            reversed(all_presents[1:d+1])) + (', and ' if d > 0 else '') + all_presents[0]
+    for verse_number in range(start_verse, end_verse + 1):
+        gifts = ', '.join(
+            reversed([('and ' if verse_number != 1 and idx == 1 else '') + gift_for_day[idx][1] for idx in
+                      range(1, verse_number + 1)]))
         ans.append(
-            f'On the {days[d]} day of Christmas my true love gave to me: {day_presents}.')
+            f'On the {gift_for_day[verse_number][0]} day of Christmas my true love gave to me: {gifts}.')
     return ans
