300e8ab8c3529fcfecb5830006418d1c60a05265
Vlad Flore <flore.vlad@gmail.com>
2021-01-23 14:27:33 +0100

    Remove reversed, make it more readable

--- python/twelve-days/twelve_days.py
+++ python/twelve-days/twelve_days.py
@@ -1,23 +1,42 @@
 def recite(start_verse, end_verse):
-    gift_for_day = {
-        1: ("first", "a Partridge in a Pear Tree"),
-        2: ("second", "two Turtle Doves"),
-        3: ("third", "three French Hens"),
-        4: ("fourth", "four Calling Birds"),
-        5: ("fifth", "five Gold Rings"),
-        6: ("sixth", "six Geese-a-Laying"),
-        7: ("seventh", "seven Swans-a-Swimming"),
-        8: ("eighth", "eight Maids-a-Milking"),
-        9: ("ninth", "nine Ladies Dancing"),
-        10: ("tenth", "ten Lords-a-Leaping"),
-        11: ("eleventh", "eleven Pipers Piping"),
-        12: ("twelfth", "twelve Drummers Drumming")
+    number_to_day = {
+        1: "first",
+        2: "second",
+        3: "third",
+        4: "fourth",
+        5: "fifth",
+        6: "sixth",
+        7: "seventh",
+        8: "eighth",
+        9: "ninth",
+        10: "tenth",
+        11: "eleventh",
+        12: "twelfth"
     }
-    ans = []
+    day_to_gift = {
+        "first": "a Partridge in a Pear Tree",
+        "second": "two Turtle Doves",
+        "third": "three French Hens",
+        "fourth": "four Calling Birds",
+        "fifth": "five Gold Rings",
+        "sixth": "six Geese-a-Laying",
+        "seventh": "seven Swans-a-Swimming",
+        "eighth": "eight Maids-a-Milking",
+        "ninth": "nine Ladies Dancing",
+        "tenth": "ten Lords-a-Leaping",
+        "eleventh": "eleven Pipers Piping",
+        "twelfth": "twelve Drummers Drumming"
+    }
+    recitation = []
     for verse_number in range(start_verse, end_verse + 1):
-        gifts = ', '.join(
-            reversed([('and ' if verse_number != 1 and idx == 1 else '') + gift_for_day[idx][1] for idx in
-                      range(1, verse_number + 1)]))
-        ans.append(
-            f'On the {gift_for_day[verse_number][0]} day of Christmas my true love gave to me: {gifts}.')
-    return ans
+        gift_list = []
+        for idx in range(verse_number, 0, -1):
+            gift = ''
+            if verse_number != 1 and idx == 1:
+                gift += 'and '
+            gift += day_to_gift[number_to_day[idx]]
+            gift_list.append(gift)
+        gifts = ', '.join(gift_list)
+        recitation.append(
+            f'On the {number_to_day[verse_number]} day of Christmas my true love gave to me: {gifts}.')
+    return recitation
