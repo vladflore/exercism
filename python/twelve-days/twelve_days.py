1a6f58d6f9c708c58606fea7ea9e207c488541bd
Vlad Flore <flore.vlad@gmail.com>
2021-11-20 22:49:19 +0100

    Adjust solution for 12 days in py

--- python/twelve-days/twelve_days.py
+++ python/twelve-days/twelve_days.py
@@ -1,32 +1,33 @@
+NUMBER_TO_DAY = {
+    1: "first",
+    2: "second",
+    3: "third",
+    4: "fourth",
+    5: "fifth",
+    6: "sixth",
+    7: "seventh",
+    8: "eighth",
+    9: "ninth",
+    10: "tenth",
+    11: "eleventh",
+    12: "twelfth"
+}
+DAY_TO_GIFT = {
+    1: "a Partridge in a Pear Tree",
+    2: "two Turtle Doves",
+    3: "three French Hens",
+    4: "four Calling Birds",
+    5: "five Gold Rings",
+    6: "six Geese-a-Laying",
+    7: "seven Swans-a-Swimming",
+    8: "eight Maids-a-Milking",
+    9: "nine Ladies Dancing",
+    10: "ten Lords-a-Leaping",
+    11: "eleven Pipers Piping",
+    12: "twelve Drummers Drumming"
+}
+
 def recite(start_verse, end_verse):
-    number_to_day = {
-        1: "first",
-        2: "second",
-        3: "third",
-        4: "fourth",
-        5: "fifth",
-        6: "sixth",
-        7: "seventh",
-        8: "eighth",
-        9: "ninth",
-        10: "tenth",
-        11: "eleventh",
-        12: "twelfth"
-    }
-    day_to_gift = {
-        "first": "a Partridge in a Pear Tree",
-        "second": "two Turtle Doves",
-        "third": "three French Hens",
-        "fourth": "four Calling Birds",
-        "fifth": "five Gold Rings",
-        "sixth": "six Geese-a-Laying",
-        "seventh": "seven Swans-a-Swimming",
-        "eighth": "eight Maids-a-Milking",
-        "ninth": "nine Ladies Dancing",
-        "tenth": "ten Lords-a-Leaping",
-        "eleventh": "eleven Pipers Piping",
-        "twelfth": "twelve Drummers Drumming"
-    }
     recitation = []
     for verse_number in range(start_verse, end_verse + 1):
         gift_list = []
@@ -34,9 +35,9 @@ def recite(start_verse, end_verse):
             gift = ''
             if verse_number != 1 and idx == 1:
                 gift += 'and '
-            gift += day_to_gift[number_to_day[idx]]
+            gift += DAY_TO_GIFT[idx]
             gift_list.append(gift)
         gifts = ', '.join(gift_list)
         recitation.append(
-            f'On the {number_to_day[verse_number]} day of Christmas my true love gave to me: {gifts}.')
+            f'On the {NUMBER_TO_DAY[verse_number]} day of Christmas my true love gave to me: {gifts}.')
     return recitation
